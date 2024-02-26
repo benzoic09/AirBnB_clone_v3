bac2aba233212429059a6a5ecc2c04f64fc8f833
kiplrry <kiplarry19@gmail.com>
2024-02-26 21:43:52 +0300

    app

--- api/v1/app.py
+++ api/v1/app.py
@@ -2,13 +2,14 @@
 """
 This module contains the principal application
 """
-from flask import Flask
 from os import getenv
+from flask import Flask
 from api.v1.views import app_views
 from models import storage
 
 app = Flask(__name__)
 
+app.config['JSONIFY_PRETTYPRINT_REGULAR'] = True
 app.register_blueprint(app_views)
 
 
@@ -23,6 +24,6 @@ def tear_down(exception):
 if __name__ == "__main__":
 
     host = getenv('HBNB_API_HOST', default='0.0.0.0')
-    port = getenv('HBNB_API_PORT', default=5000)
+    port = getenv('HBNB_API_PORT', default='5000')
 
     app.run(host=host, port=port, threaded=True)
