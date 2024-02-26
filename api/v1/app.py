6cf031db3cd97bfa32842f6be197eaaa5c55a518
kiplrry <kiplarry19@gmail.com>
2024-02-26 19:36:17 +0300

    app

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
-"""This module contains the principal application
 """
-
-
+This module contains the principal application
+"""
 from flask import Flask
 from os import getenv
 from api.v1.views import app_views
@@ -15,12 +14,15 @@ app.register_blueprint(app_views)
 
 @app.teardown_appcontext
 def tear_down(exception):
-    """This def calls storage.close()
+    """
+    This def calls storage.close()
     """
     storage.close()
 
 
 if __name__ == "__main__":
-    host = getenv('HBNB_API_HOST') or '0.0.0.0'
-    port = getenv('HBNB_API_PORT') or 5000
+
+    host = getenv('HBNB_API_HOST', default='0.0.0.0')
+    port = getenv('HBNB_API_PORT', default=5000)
+
     app.run(host=host, port=port, threaded=True)
